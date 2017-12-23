45622a608d764d9f229c553f07fb814d810fde33
Kim L By <frostsylvia@gmail.com>
2017-12-23 18:20:47 +1100

    set up gulp file for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 'use strict';
 
 // dependencies
-/*
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
-var minifyCSS = require('gulp-uglify');
+var minifyCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var changed = require('gulp-changed');
-*/
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
 
 /////////////////////////////////////////////////////
 ///////////        CSS/CSS      ///////////////
@@ -21,38 +16,28 @@ var SCSS_SRC = './src/Assets/scss/**/*.scss';
 var SCSS_DEST = './src/Assets/css';
 
 // compile SCSS
-/*
-gulp.task('compile_scss', function(){
-
+gulp.task('compile:scss', function(){
   gulp.src(SCSS_SRC)
   .pipe(sass().on('error', sass.logError))
-  .pipe(minifyCSS())
-  .on('error',function(err){ gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+  .pipe(minifyCSS().on('error', function(err){
+    gutil.log(gutil.colors.red('[Error]'),err.toString());
+  }))
   .pipe(rename( {suffix: '.min'} ))
   .pipe(changed(SCSS_DEST))
   .pipe(gulp.dest(SCSS_DEST));
 
 });
-*/
 
-gulp.task('sass', function () {
+/*gulp.task('compile:scss', function () {
   return gulp.src(SCSS_SRC)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(SCSS_DEST));
-});
+});*/
 
 // detect changes in SCSS
-/*
-gulp.task('watch_scss', function(){
-  gulp.watch(SCSS_SRC, ['compile_scss']);
-});
-*/
-
 gulp.task('sass:watch', function () {
-  gulp.watch(SCSS_SRC, ['sass']);
+  gulp.watch(SCSS_SRC, ['compile:scss']);
 });
 
 // run tasks
-/*gulp.task('default', ['watch_scss']);*/
-
 gulp.task('default',['sass:watch']);
