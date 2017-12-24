62b81ad170b8ba37748cc87c8b062ec9f5984ab5
Kim L By <frostsylvia@gmail.com>
2017-12-24 20:30:52 +1100

    Basic React Webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
+// install the package
+  // npm i --dev---save react-router-dom
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+// components
+import Header from './components/headerComponents/header';
+import Footer from './components/footerComponents/footer';
+import HomePage from './components/pages/homePage';
+import Products from './components/pages/products';
+
+// Include
+import './Assets/css/default.min.css';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      TEST
-      </div>
+      <Router>
+        <div className="App">
+
+          <Header />
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/products' component={Products} />
+          <Footer />
+
+        </div>
+      </Router>
     );
   }
 }
